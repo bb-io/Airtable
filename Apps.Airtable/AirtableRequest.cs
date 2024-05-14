dfecccf2cc20ff495255dc00fb11e83a7db46f2d
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-05-14 12:27:06 +0200

    Removed dev key

--- Apps.Airtable/AirtableRequest.cs
+++ Apps.Airtable/AirtableRequest.cs
@@ -12,8 +12,6 @@ public class AirtableRequest : BlackBirdRestRequest
 
     protected override void AddAuth(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
     {
-        // TODO REVERT
-        this.AddHeader("Authorization", "Bearer patZ3gvhcLiSoUtn4.bec67bffbf4171e043e85e11b6b4ebd603c4aa8a86b3b02383d8f178901f16e4");
-        // this.AddHeader("Authorization", authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value);
+        this.AddHeader("Authorization", authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value);
     }
 }
